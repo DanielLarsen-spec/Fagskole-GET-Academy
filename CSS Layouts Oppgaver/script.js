8751f5ab4c9c62199eaad66ebd5cacb8eeeff694
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-01-24 12:59:08 +0100

    Fagskole

---
+++ CSS Layouts Oppgaver/script.js
@@ -0,0 +1,31 @@
+updateView();
+function updateView()
+{
+    document.getElementById('app').innerHTML = /*html*/`
+    
+    <div class="container">
+    |<img src="img/Rooney.jpg">
+     <div></div>   
+     <div></div>   
+     <div></div>   
+     <div></div>   
+    
+    </div>
+    
+    
+    
+    
+    `;
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
