6d84957bf3b63ce165c5337b11834dc13ed98eb7
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-01-26 21:21:56 +0100

    Update finish

--- CSS Layouts Oppgaver/script.js
+++ CSS Layouts Oppgaver/script.js
@@ -1,30 +1,130 @@
-updateView();
-function updateView()
-{
-    document.getElementById('app').innerHTML = /*html*/`
+showPictureUpperLeft();
+function showPictureUpperLeft() {
+  document.getElementById("app").innerHTML = /*html*/ `
+    <h1 class="header">Flytt rundt på Wayne Rooney!</h1>
     
     <div class="container">
-    |<img src="img/Rooney.jpg">
-     <div></div>   
-     <div></div>   
-     <div></div>   
-     <div></div>   
-    
+        <div>
+            <img src="img/Rooney.jpg">
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
     
-    
-    
+    <div class="buttons">
+    <div></div>
+    <button disable>▲</button>
+    <div></div>
+    <button disable>◀</button>
+    <div></div>
+    <button onclick="showPictureUpperRight()">▶</button>
+    <div></div>
+    <button onclick="showPictureDownLeft()">▼</button>
+    </div>
     
     `;
-    
-    
-    
-    
-    
-    
-    
-    
 }
+
+function showPictureUpperRight(){
+document.getElementById('app').innerHTML = /*HTML*/`
+
+<div class="container">
+<div>
+</div><div>
+<img src="img/Rooney.jpg">
+</div>
+
+<div></div>
+<div></div>
+</div>
+
+<div class="buttons">
+<div></div>
+<button disable>▲</button>
+<div></div>
+<button onclick="showPictureUpperLeft()">◀</button>
+<div></div>
+<button>▶</button>
+<div></div>
+<button onclick="showPictureDownRight()">▼</button>
+</div>
+`;
+}
+
+function showPictureDownLeft(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+
+    <div class="container">
+    <div></div>
+    <div>
+    </div><div>
+    
+    <img src="img/Rooney.jpg">
+    </div>
+    
+    <div>
+    
+    </div>
+    </div>
+    
+    <div class="buttons">
+    <div></div>
+    <button onclick="showPictureUpperLeft()">▲</button>
+    <div></div>
+    <button disable>◀</button>
+    <div></div>
+    <button onclick="showPictureDownRight()">▶</button>
+    <div></div>
+    <button disable>▼</button>
+    </div>
+    `; 
+}
+
+function showPictureDownRight(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+
+    <div class="container">
+    <div>
+    </div><div>
+    </div>
+    
+    <div></div>
+    <div>
+    <img src="img/Rooney.jpg">
+    
+    </div>
+    </div>
+    
+    <div class="buttons">
+    <div></div>
+    <button onclick="showPictureUpperRight()">▲</button>
+    <div></div>
+    <button onclick="showPictureDownLeft()">◀</button>
+    <div></div>
+    <button>▶</button>
+    <div></div>
+    <button>▼</button>
+    </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
