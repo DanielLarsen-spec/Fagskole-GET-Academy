e4a961f997082a9d3dcb2a4b41ae59def7188c20
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-02-04 12:42:30 +0100

    Update script.js

--- JS Oppgave121C/script.js
+++ JS Oppgave121C/script.js
@@ -1,10 +1,10 @@
 updateView();
 function updateView(){
     addInputText('fname', 'lname');
-    addInputRange();
-    addInputNumber();
-    addInputRadio();
-    addInputCheckbox();
+    addInputRange('zero2hundred');
+    addInputNumber('number');
+    addInputRadio('Html', 'Css', 'Javascript');
+    addInputCheckbox('vehicle1', 'vehicle2', 'vehicle3');
 }
 //Sette alt inn i sammem funskjon //
 function addInputText(forname, lastname){
@@ -14,61 +14,61 @@ function addInputText(forname, lastname){
     `;
     
     document.getElementById('app').innerHTML += /*HTML*/`
-    <label for="$">Last name:</label><br/>
-    <input type="text" id="${addInputTextIdLastname}"><br/>
+    <label for="${lastname}">Last name:</label><br/>
+    <input type="text" id="${lastname}"><br/>
     `;
 }
     
-function addInputRange(){
+function addInputRange(rangeNumber){
     document.getElementById('app').innerHTML += /*HTML*/`
-    <label for="zero2hundred">En til hundre:</label><br/>
-    <input type="range" min="1" max="100" step="1" id="zero2hundred"><br/>
+    <label for="${rangeNumber}">En til hundre:</label><br/>
+    <input type="range" min="1" max="100" step="1" id="${rangeNumber}"><br/>
     `;
 }
     
  
-function addInputNumber(){
+function addInputNumber(inputNumber){
     document.getElementById('app').innerHTML += /*HTML*/`
-    <label for="number">Skriv inn et tall:</label><br/>
-    <input type="number" id="number"><br/>
+    <label for="${inputNumber}">Skriv inn et tall:</label><br/>
+    <input type="number" id="${inputNumber}"><br/>
     `;
 }
     
-function addInputRadio(){
+function addInputRadio(html, css, js){
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="radio" id="html" name="fav_language">
-    <label for="html">HTML</label><br/>
+    <input type="radio" id="${html}" name="fav_language">
+    <label for="${html}">HTML</label><br/>
     `;
     
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="radio" id="css" name="fav_language">
-    <label for="css">CSS</label><br>
+    <input type="radio" id="${css}" name="fav_language">
+    <label for="${css}">CSS</label><br>
     `;
     
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="radio" id="javascript" name="fav_language">
-    <label for="javascript">JavaScript</label><br/>
+    <input type="radio" id="${js}" name="fav_language">
+    <label for="${js}">JavaScript</label><br/>
     `;
 }
 
-function addInputCheckbox(){
+function addInputCheckbox(bike, car, boat){
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="checkbox" id="vehicle1">
-    <label for="vehicle1"> I have a bike</label><br/>
+    <input type="checkbox" id="${bike}">
+    <label for="${bike}"> I have a bike</label><br/>
     `;
 
 
 
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="checkbox" id="vehicle2">
-    <label for="vehicle2"> I have a car</label><br/>
+    <input type="checkbox" id="${car}">
+    <label for="${car}"> I have a car</label><br/>
     `;
 
 
 
     document.getElementById('app').innerHTML += /*HTML*/`
-    <input type="checkbox" id="vehicle3">
-    <label for="vehicle3"> I have a boat</label><br/>
+    <input type="checkbox" id="${boat}">
+    <label for="${boat}"> I have a boat</label><br/>
     `;
 
 }
